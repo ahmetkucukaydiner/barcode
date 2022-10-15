651e0f5393cfee129b96b71d9d93a8ad2a43a6ea
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2022-10-15 16:34:17 +0300

    Add project files.

---
+++ barcode/Operation.cs
@@ -0,0 +1,25 @@
+﻿using BarcodeLib;
+using System;
+
+namespace barcode
+{
+    internal class Operation
+    {
+        static TYPE type = TYPE.PHARMACODE;
+
+        public static Barcode CreateAndSave(string data)
+        {
+            Barcode barcodee = new Barcode();
+            barcodee.Encode(type, data);
+
+            barcodee.SaveImage(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\barcode.png", BarcodeLib.SaveTypes.PNG);
+            Console.WriteLine("Barkod oluşturuldu. Data saklandı. Saklanan data = {0}", data);
+            return barcodee;
+        }
+
+        public static string ReadBarcode(Barcode barcodee)
+        {
+            return "Barkod = " + barcodee.RawData + " \n";
+        }
+    }
+}
---
+++ barcode/Program.cs
@@ -0,0 +1,19 @@
+﻿using BarcodeLib;
+using System;
+
+namespace barcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Barcode barcode = Operation.CreateAndSave("777");
+             Console.WriteLine(Operation.ReadBarcode(barcode)); */
+
+
+            Barcode barcode = Operation.CreateAndSave("17698");
+            Console.WriteLine(Operation.ReadBarcode(barcode));
+
+        }
+    }
+}
